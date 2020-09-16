0e3e5cd373264fffdd1d3eb8a9ea33ceb2dbf0cd
hershler <hra@sem-wolf.org>
2020-09-16 13:28:54 -0700

    Tests are running completely

--- test.c
+++ test.c
@@ -1,36 +1,38 @@
-//
-// Created by a on 6/17/20.
-//
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 int testCpp2c(char* type){
 
-	char path[25];
-    char* gcc[] = {"gcc", "-ansi", "-pedantic", "-o", "outc", "", "", NULL};
-	char* outc[] = {"./outc", ">", "out_c.txt", NULL};
+	char gcc5[100], gcc6[100], gpp5[100], gpp6[100];
+	char* touch[] = {"touch", "outc.txt", "outcpp.txt", NULL};
+    	char* gcc[] = {"gcc", "-ansi", "-pedantic", "-o", "outc", "", "", NULL};
+	char* outc[] = {"./outc", "outc.txt", NULL};
 	char* gpp[] = {"g++", "-ansi", "-pedantic", "-o", "outcpp", "", "", NULL};
-	char* outcpp[] = {"./outcpp", ">", "out_cpp.txt", NULL};
-	char* diff[] = {"diff", "out_c.txt", "out_cpp.txt", NULL};
-	char** commands[] = {gcc, outc, gpp, outcpp};
+	char* outcpp[] = {"./outcpp", "outcpp.txt", NULL};
+	char* diff[] = {"diff", "outc.txt", "outcpp.txt", NULL};
+	char* rm[] = {"rm", "outc.txt", "outcpp.txt", "outc", "outcpp", NULL};
+	char** commands[] = {touch, gcc, outc, gpp, outcpp, diff, rm};
 	int status;
-	int commandsLen = 5;
+	int commandsLen = 7;
 
-	path[0] = '\0';
-	gcc[5] = strcat(strcat(strcat(strcat(path, type), "/c/cpp2c_"), type), ".c");
-	path[0] = '\0';
-	gcc[6] = strcat(strcat(strcat(strcat(path, type), "/c/cpp2c_"), type), "_defs.c");
-	path[0] = '\0';
-	gpp[5] = strcat(strcat(strcat(strcat(path, type), "/cpp/cpp2c_"), type), ".cpp");
-	path[0] = '\0';
-	gpp[6] = strcat(strcat(strcat(strcat(path, type), "/cpp/cpp2c_"), type), "_defs.cpp");
+	gcc5[0] = '\0';
+	gcc[5] = strcat(strcat(strcat(strcat(gcc5, type), "/c/cpp2c_"), type), ".c");
+	gcc6[0] = '\0';
+	gcc[6] = strcat(strcat(strcat(strcat(gcc6, type), "/c/cpp2c_"), type), "_defs.c");
+	gpp5[0] = '\0';
+	gpp[5] = strcat(strcat(strcat(strcat(gpp5, type), "/cpp/cpp2c_"), type), ".cpp");
+	gpp6[0] = '\0';
+	gpp[6] = strcat(strcat(strcat(strcat(gpp6, type), "/cpp/cpp2c_"), type), "_defs.cpp");
 
 	for(int i = 0; i < commandsLen; ++i){
+
 		pid_t pid = fork();
 
 		if(-1 == pid){
@@ -38,7 +40,22 @@ int testCpp2c(char* type){
 			exit(-1);
 		}
 		if(0 == pid){
-			execvp(commands[i][0], commands[i]);
+			if(commands[i] != outc && commands[i] != outcpp){
+				execvp(commands[i][0], commands[i]);
+			}
+			else{
+				int fd;
+
+				if((fd = open(commands[i][0] == "./outc"? "outc.txt": "outcpp.txt", O_RDWR | O_CREAT))==-1){
+				  perror("open");
+				  return 1;
+				}
+
+				dup2(fd,STDOUT_FILENO);
+				dup2(fd,STDERR_FILENO);
+				close(fd);
+				execvp(commands[i][0], commands[i]);
+			}
 		}
 		else{
 			waitpid(pid, &status, 0);
@@ -49,22 +66,40 @@ int testCpp2c(char* type){
 }
 
 int testPolymorphism(){
-	return testCpp2c("polymorphism");
+
+	printf("\n\n\t\t\tWe are testing the polymorphism\n\
+			Now you'll see all differences (should be nothing...) cpp program vs c program\n\
+			If you see warnings just ignore them, it's under the hood, don't worry... ;)\n\n\n");
+	if(testCpp2c("polymorphism")){
+		printf("oops something got wrong\n");
+	}
 }
 
 int testInheritance(){
-	return testCpp2c("inheritance");
+
+	printf("\n\n\t\t\tWe are testing the inheritane\n\
+			Now you'll see all differences (should be nothing...) cpp program vs c program\n\
+			If you see warnings just ignore them, it's under the hood, don't worry... ;)\n\n\n");
+	if(testCpp2c("inheritance")){
+		printf("oops something got wrong\n");
+	}
 }
 
-int testEncapsulation(){
-	return testCpp2c("encapsulation");
+void testEncapsulation(){
+
+	printf("\n\n\t\t\tWe are testing the encapsulation\n\
+			Now you'll see all differences (should be nothing...) cpp program vs c program\n\
+			If you see warnings just ignore them, it's under the hood, don't worry... ;)\n\n\n");
+	if(testCpp2c("encapsulation")){
+		printf("oops something got wrong\n");
+	}
 }
 
 
 int main(){
 
-	printf("%d\n", testPolymorphism());
-	printf("%d\n", testInheritance());
-	printf("%d\n", testEncapsulation());
-
+	testPolymorphism();
+	testInheritance();
+	testEncapsulation();
+	return 0;
 }
